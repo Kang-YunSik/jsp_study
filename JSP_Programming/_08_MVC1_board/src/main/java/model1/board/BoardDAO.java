bdf5fb481ee1a28e26ec526757ab84fd66a74677
kys <kang_ys_1@naver.com>
2023-11-14 13:40:17 +0900

    feat : 게시물 목록을 가져오는 기능 추가

--- JSP_Programming/_08_MVC1_board/src/main/java/model1/board/BoardDAO.java
+++ JSP_Programming/_08_MVC1_board/src/main/java/model1/board/BoardDAO.java
@@ -3,7 +3,9 @@ package model1.board;
 import model1.common.JDBConnect;
 
 import javax.servlet.ServletContext;
+import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 
 public class BoardDAO extends JDBConnect {
@@ -25,6 +27,7 @@ public class BoardDAO extends JDBConnect {
                     + " LIKE '%" + map.get("searchWord") + "%'";
         }
 
+        // 쿼리문 실행
         try{
             stmt = con.createStatement();   // 쿼리문 생성
             rs = stmt.executeQuery(query);  // 쿼리 실행
@@ -36,5 +39,44 @@ public class BoardDAO extends JDBConnect {
             e.printStackTrace();
         }
         return totalCount;  // JSP로 반환할 값
+    }
+
+    // 게시물 목록을 가져오는 메소드
+    public List<BoardDTO> selectList(Map<String, Object> map) {
+        List<BoardDTO> bbs = new Vector<BoardDTO>();    // 결과(게시물 목록)를 담을 변수
+
+        // 게시물 목록을 가져오는 쿼리문 작성
+        String query = "SELECT * FROM board ";
+        // selectCount()와 같은 원리
+        if (map.get("searchWord") != null) {
+            query += " WHERE " + map.get("searchField") + " "
+                    + " LIKE '%" + map.get("searchWord") + "%'";
+            query += " ORDER BY num DESC "; // 최근 게시물을 상단에 표시
+        }
+
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+
+            while (rs.next()) { // 결과를 순회하며 더 이상 행이 존재하지 않을 때까지 수행
+
+                BoardDTO dto = new BoardDTO();  // dto 객체 초기화 ???
+                // 쿼리문 결과(게시글 1개)의 내용을 DTO의 값으로 저장
+                dto.setNum(rs.getString("num"));
+                dto.setTitle(rs.getString("title"));
+                dto.setContent(rs.getString("content"));
+                dto.setPostdate(rs.getDate("postdate"));
+                dto.setId(rs.getString("id"));
+                dto.setVisitcount(rs.getString("visitcount"));
+
+                bbs.add(dto);   // 결과(게시물 1개)를 dto에 저장
+            }
+
+        } catch (Exception e) {
+            System.out.println("게시물 조회 중 예외 발생");
+            e.printStackTrace();
+        }
+
+        return bbs; // JSP로 반환할 값
     }
 }
